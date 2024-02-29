d223bfcd055c61459f2e4c8233f22f1f1d0c8936
Koray <koray.uymaz@agesa.com.tr>
2024-02-29 14:59:30 +0300

    server action for createBook added

--- src/components/add-book.tsx
+++ src/components/add-book.tsx
@@ -5,7 +5,8 @@ import {
   PopoverContent,
   Button,
 } from "@nextui-org/react";
-import BookState from "@/components/book-state";
+
+import { createBook } from "@/actions/create-book";
 
 export default function AddBook() {
   return (
@@ -14,11 +15,12 @@ export default function AddBook() {
         <Button color="primary">New Book</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form>
+        <form action={createBook}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <Input name="name" label="Book Name" labelPlacement="outside" />
             <Input name="author" label="Author" labelPlacement="outside" />
             <Input name="state" label="Book State" labelPlacement="outside" />
+            <Button type="submit">Submit</Button>
           </div>
         </form>
       </PopoverContent>
