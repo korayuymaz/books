245dbce1dbd22a1e41eaf1977a5a04efbf5813c6
Koray <koray.uymaz@agesa.com.tr>
2024-02-29 14:27:29 +0300

    Add book page created

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,13 +11,16 @@ export default function Header() {
   return (
     <Navbar className="shadow mb-6">
       <NavbarBrand>
-        <Link href="/" className="font-bold">
+        <Link href="/" className="font-bold text-xl">
           Home
         </Link>
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <Link href="/my-books" className="font-bold">
+        <Link href="/my-books" className="font-semibold text-zinc-600">
           My Books
+        </Link>
+        <Link href="/add-book" className="font-semibold text-zinc-600">
+          Add Book
         </Link>
       </NavbarContent>
     </Navbar>
