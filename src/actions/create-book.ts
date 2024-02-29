07f903bd4990c7ff405b5e35bf2f878883b18ec0
Koray <koray.uymaz@agesa.com.tr>
2024-02-29 15:11:56 +0300

    Zod library added

--- src/actions/create-book.ts
+++ src/actions/create-book.ts
@@ -1,7 +1,21 @@
 "use server";
 
+import { z } from "zod";
+
+const createBookSchema = z.object({
+  name: z.string().min(3),
+  author: z.string().min(3),
+  state: z.string().min(5),
+});
+
 export async function createBook(formData: FormData) {
-  const name = formData.get("name");
-  const author = formData.get("author");
-  const state = formData.get("state");
+  const result = createBookSchema.safeParse({
+    name: formData.get("name"),
+    author: formData.get("author"),
+    state: formData.get("state"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
 }
